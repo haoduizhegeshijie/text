6ca059079d89e446b2f76e2f2f7e9baf423fe5e5
安宁 <562378177@qq.com>
2021-01-22 10:20:04 +0800

    Revert "版本更新"
    This reverts commit e2cab67d

--- src/main.js
+++ src/main.js
@@ -1,21 +1,14 @@
 import Vue from 'vue'
 import App from './App'
-import request from './common/request.js'
-import api from './api/index.js'
-import url from './common/config.js'
 import uView from "uview-ui";
 import { Checkbox, CheckboxGroup, Stepper } from 'vant'
 Vue.use(Checkbox).use(CheckboxGroup).use(Stepper).use(uView)
 
 Vue.config.productionTip = false
-Vue.prototype.$request = request
-Vue.prototype.$api = api
-Vue.prototype.$url = url
 
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App,
-  store
+  ...App
 })
 app.$mount()
